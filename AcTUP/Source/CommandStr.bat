f46eba075a8ee0e792e9f465d16e5a8d76b319a5
邢子晗 <mrsingorxing@gmail.com>
2024-11-26 18:17:27 +0800

    Initial commit

---
+++ AcTUP/Source/CommandStr.bat
@@ -0,0 +1,2 @@
+copy SHDFIL "D:\Program Files\CMST Software\AcTUP v2.2L\AcTUP\Output\Default\Bounce+Bellhop\Test2_00500.shd" >> "D:\Program Files\CMST Software\AcTUP v2.2L\AcTUP\Output\Default\Bounce+Bellhop\Test2_00500.prn"
+del SHDFIL
